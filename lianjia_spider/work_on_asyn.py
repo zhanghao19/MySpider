cf5f76c7259d9a4ea1760fa14ec8f61e551c0790
zhanghao19 <zh1996@outlook.com>
2019-11-13 23:21:24 +0800

    多线程爬取优化完成

--- lianjia_spider/work_on_asyn.py
+++ lianjia_spider/work_on_asyn.py
@@ -10,7 +10,7 @@ from pymongo import MongoClient
 
 
 class LianJia:
-    def __init__(self, part, page=1):
+    def __init__(self, part=None, page=1):
         self.executor = ThreadPoolExecutor(max_workers=2)  # 使用内置线程池, 设置最大线程数
 
         # 声明Chrome浏览器对象
@@ -25,12 +25,14 @@ class LianJia:
         self.db = self.client.LianJia
         self.collection = self.db.houseInfo
 
-    @staticmethod
+    @staticmethod   # 因为没有引入类中的变量, 所以建议写成静态方法
     def write_item_to_json(item):
         """写入json文件"""
+        # 将item字典转换为json格式, ensure_ascii为否, 表示返回值可以包含非ascii值, 比如汉字
         json_data = json.dumps(item, ensure_ascii=False)
-        print(item)
+        # a表示每次写入为追加, encoding是为了不让中文乱码
         with open('data.json', 'a', encoding='utf-8') as f:
+            # 执行写入, 尾部追加换行符
             f.write(json_data + '\n')
         print(f'>>>[{item["title"]}]写入成功')
 
@@ -41,6 +43,7 @@ class LianJia:
         return {now: obj}
 
     def write_item_to_mongo(self, item):
+        """插入item到数据库"""
         self.collection.insert_one(item)
         print(f'>>>[{item["title"]}]写入成功')
 
@@ -52,7 +55,7 @@ class LianJia:
         item['title'] = driver.find_element_by_tag_name('h1').text  # 标题
         price = driver.find_element_by_css_selector('span.total').text  # 价格
         first_price = self.clock(price)  # 第一次的价格
-        item['price'] = [first_price]  # 房子的价格, 可增量爬取
+        item['price'] = [first_price]  # 房子的价格与更新日期
         house_info = driver.find_elements_by_css_selector('div.mainInfo')
         item['room'] = house_info[0].text  # 户型
         item['faceTo'] = house_info[1].text  # 朝向
@@ -68,12 +71,12 @@ class LianJia:
         self.executor.submit(self.house_detail, item=dict(item), url=url_list[0], driver=self.driver2)
         self.executor.submit(self.house_detail, item=dict(item), url=url_list[1], driver=self.driver3)
 
-    def house_list(self, item, driver):
+    def house_list(self, item):
         """获取一个城区中所有房子的详情页链接"""
         for page in range(1, self.page + 1):
-            driver.get(item['partURL'] + f'pg{page}co32/')  # 访问城区的页面
+            self.driver.get(item['partURL'] + f'pg{page}co32/')  # 访问城区的页面
             # 获取到所有的房子链接
-            house_ls = driver.find_elements_by_xpath('//ul[@class="sellListContent"]//div[@class="title"]/a')
+            house_ls = self.driver.find_elements_by_xpath('//ul[@class="sellListContent"]//div[@class="title"]/a')
             # 生成url列表
             house_url_ls = [house.get_attribute("href") for house in house_ls]
             # 循环内的作用， 同时给url_list参数提供两个不同的值
@@ -87,24 +90,43 @@ class LianJia:
 
     def run(self):
         """获取所有城区的页面链接"""
-        self.driver.get('https://wh.lianjia.com/ershoufang/')  # 访问二手房网址
+        # 访问二手房网址
+        self.driver.get('https://wh.lianjia.com/ershoufang/')
         # 获取所有城区的元素对象
         temp_ls = self.driver.find_elements_by_xpath('//div[@class="position"]/dl[2]/dd/div[1]/div/a')
+        if self.part:
+            self.get_one_part(temp_ls, self.part)
+        else:
+            self.get_all_part(temp_ls)
         # 城区名和url组成键值对
+
+    def get_one_part(self, temp_ls, part):
+        """获取一个城区的房子"""
         part_dict = {ele.text: ele.get_attribute("href") for ele in temp_ls}
         try:
             # 初始化一个容器, 用来存放房子的信息
-            item = {'partName': self.part, 'partURL': part_dict[self.part]}
-            self.house_list(dict(item), self.driver)  # 传递深拷贝的item对象
+            item = {'partName': part, 'partURL': part_dict[part]}
+            self.house_list(dict(item))  # 传递深拷贝的item对象
         except KeyError:
             print(f'请指定有效的城区名, 如下：\n{list(part_dict.keys())}')
+
+    def get_all_part(self, temp_ls):
+        """获取所有城区的房子"""
+        # 城区名 集
+        part_name_ls = [ele.text for ele in temp_ls]
+        # 城区链接 集
+        part_url_ls = [ele.get_attribute("href") for ele in temp_ls]
+        item = {}  # 初始化一个容器, 用来存放房子的信息
+        for i in range(len(temp_ls)):
+            item['partName'] = part_name_ls[i]  # 城区名
+            item['partURL'] = part_url_ls[i]  # 城区页面链接
+            self.house_list(dict(item))  # 传递深拷贝的item对象
 
     def __del__(self):
         self.driver.close()  # 关闭浏览器1
         self.driver2.close()  # 关闭浏览器2
         self.driver3.close()  # 关闭浏览器3
         print('>>>>[Well Done]')
-
 
 if __name__ == '__main__':
     lj = LianJia(part='江岸', page=1)
